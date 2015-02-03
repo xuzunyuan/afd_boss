d4b125d70fa620f805532795b552f3f4d3385bbd
xuzunyuan <xuzunyuan@yiwang.com>
2015-02-03 15:02:38 +0800

    v

--- src/main/java/com/afd/boss/web/LoginController.java
+++ src/main/java/com/afd/boss/web/LoginController.java
@@ -8,8 +8,6 @@ import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,9 +22,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
  */
 @Controller
 public class LoginController {
-	private final static Logger logger = LoggerFactory
-			.getLogger(LoginController.class);
-
 	/**
 	 * 进入登录界面
 	 * 
